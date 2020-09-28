b485ee5f7f53773714f1346c052c2faf70715847
chelseachen007 <aqwejie@qq.com>
2020-09-28 10:50:44 +0800

    fix: 防抖节流

--- 防抖节流/debounce.js
+++ 防抖节流/debounce.js
@@ -9,72 +9,46 @@
 var count = 1;
 var container = document.getElementById('container');
 
-function getUserAction() {
-    container.innerHTML = count++;
+function getUserAction () {
+  container.innerHTML = count++;
 };
 
 //不绑定this 会指向window
 //需要传参
-function debounce(func, wait, immediate = true) {
+// function debounce (func, wait, immediate = true) {
 
-    var timeout, result;
+//   var timeout, result;
 
-    return function () {
-        var context = this;
-        var args = arguments;
+//   return function () {
+//     var context = this;
+//     var args = arguments;
 
-        if (timeout) clearTimeout(timeout);
-        if (immediate) {
-            // 如果已经执行过，不再执行
-            var callNow = !timeout;
-            timeout = setTimeout(function () {
-                timeout = null;
-            }, wait)
-            if (callNow) result = func.apply(context, args)
-        } else {
-            timeout = setTimeout(function () {
-                func.apply(context, args)
-            }, wait);
-        }
-        return result;
-    }
-}
+//     if (timeout) clearTimeout(timeout);
+//     if (immediate) {
+//       // 如果已经执行过，不再执行
+//       var callNow = !timeout;
+//       timeout = setTimeout(function () {
+//         timeout = null;
+//       }, wait)
+//       if (callNow) result = func.apply(context, args)
+//     } else {
+//       timeout = setTimeout(function () {
+//         func.apply(context, args)
+//       }, wait);
+//     }
+//     return result;
+//   }
+// }
 container.onmousemove = debounce(getUserAction, 1000);
 
-function throttle(func, wait) {
-    let time = 0
-    let args = arguments
-    return function () {
-        let date = +new Date()
-        let context = this;
-        if (date - time > wait) {
-            func.apply(context, args)
-            time = date
-        }
-    }
-}
-//第二种
-function throttle(func, wait) {
-    let args = arguments
-    return function () {
-        let timeout = null
-        let context = this;
-
-        if (!timeout) {
-            timeout = setTimeout(() => {
-                func.apply(context, args)
-                timeout = null
-            }, wait);
-        }
-
-    }
-}
-
-
-
-function throttle(func,wait){
-    let args=[...arguments]
-    return function (){
-        
-    }
+function debounce (func, wait) {
+  let timer
+  return function () {
+    let arg = arguments
+    let context = this
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func.apply(context, arg)
+    }, wait)
+  }
 }
--- 防抖节流/throttle.js
+++ 防抖节流/throttle.js
@@ -1,67 +1,64 @@
 var count = 1;
 var container = document.getElementById("container");
 
-function getUserAction() {
+function getUserAction () {
   container.innerHTML = count++;
 }
 
-// container.onmousemove = getUserAction;
-// 第一版
-// function throttle(func, wait) {
-//   var context, args;
+// function throttle (func, wait) {
+//   var context, args, timeout;
 //   var previous = 0;
-
-//   return function () {
+//   var throttled = function () {
 //     var now = +new Date();
 //     context = this;
 //     args = arguments;
-//     if (now - previous > wait) {
-//       func.apply(context, args);
-//       previous = now;
+//     function later () {
+//       previous = +new Date();
+//       timeout = null;
 //     }
-//   };
-// }
-// function throttle(func, wait) {
-//   var context, args;
-//   var timeout;
-//   var throttled = function () {
-//     context = this;
-//     args = arguments;
+//     //还有多少剩余时间
+//     let newDate = wait - (now - previous);
 
-//     if (!timeout) {
-//       timeout = setTimeout(() => {
-//         func.apply(context, args);
+//     if (newDate < 0 || newDate > wait) {
+//       if (timeout) {
+//         clearTimeout(timeout);
 //         timeout = null;
-//       }, wait);
+//       }
+//       previous = now;
+//       func.apply(context, args);
+//     } else if (!timeout) {
+//       timeout = setTimeout(later, wait);
 //     }
 //   };
 //   return throttled;
 // }
-function throttle(func, wait) {
-  var context, args, timeout;
-  var previous = 0;
-  var throttled = function () {
-    var now = +new Date();
-    context = this;
-    args = arguments;
-    function later() {
-      previous = +new Date();
-      timeout = null;
-    }
-    //还有多少剩余时间
-    let newDate = wait - (now - previous);
-
-    if (newDate < 0 || newDate > wait) {
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      previous = now;
-      func.apply(context, args);
-    } else if (!timeout) {
-      timeout = setTimeout(later, wait);
-    }
-  };
-  return throttled;
-}
 container.onmousemove = throttle(getUserAction, 1000);
+
+function throttle (func, wait) {
+  let arg, context, timeout
+  return function () {
+    context = this
+    arg = arguments
+    if (!timeout) {
+      timeout = setTimeout(() => {
+        func.apply(context, arg)
+        timeout = null
+      }, wait)
+    }
+  }
+}
+
+
+function throttle (func, wait) {
+  let arg, context, timeout
+  let date = 0
+  return function () {
+    arg = arguments
+    context = this
+    let time = new Date()
+    if (time - date > wait) {
+      func.apply(context, arg)
+      date = time
+    }
+  }
+}
