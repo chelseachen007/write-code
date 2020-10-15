e16c74a6b64808b67b638b782cef8be0f140a4a0
chelseachen007 <aqwejie@qq.com>
2020-10-15 14:45:24 +0800

    feat: this bind

---
+++ js-base-api/bind.js
@@ -0,0 +1,50 @@
+
+//实现原理:
+//返回一个可以传参的函数
+//通过构造函数继承 实现new之后 this丢失问题
+
+
+Function.prototype.mybind = function (context) {
+  if (typeof this !== "function") {
+    throw new Error("Function.prototype.bind - what is trying to be bound is not callable");
+  }
+  var self = this;
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  var fbound = function () {
+    var bindArgs = Array.prototype.slice.call(arguments);
+    // 当作为构造函数时，this 指向实例，self 指向绑定函数，因为下面一句 `fbound.prototype = this.prototype;`，已经修改了 fbound.prototype 为 绑定函数的 prototype，此时结果为 true，当结果为 true 的时候，this 指向实例。
+    // 当作为普通函数时，this 指向 window，self 指向绑定函数，此时结果为 false，当结果为 false 的时候，this 指向绑定的 context。
+    self.apply(this instanceof self ? this : context, args.concat(bindArgs));
+  }
+
+  // 修改返回函数的 prototype 为绑定函数的 prototype，实例就可以继承函数的原型中的值
+  fbound.prototype = this.prototype;
+  return fbound;
+}
+
+var value = 2;
+
+var foo = {
+  value: 1
+};
+
+function bar (name, age) {
+  this.habit = 'shopping';
+  console.log(this.value);
+  console.log(name);
+  console.log(age);
+}
+
+bar.prototype.friend = 'kevin';
+
+var bindFoo = bar.mybind(foo, 'daisy');
+
+var obj = new bindFoo('18');
+// undefined
+// daisy
+// 18
+console.log(obj.habit);
+console.log(obj.friend);
+// shopping
+// kevin
---
+++ js-base-api/call.js
@@ -0,0 +1,24 @@
+// 实现原理 在对象内部添加一个函数 执行参数 然后删除
+Function.prototype.myCall = function (context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('Error');
+  }
+  context.fn = this
+  const arg = [...arguments].slice(1)
+  const result = context.fn(...arg)
+  delete context.fn
+  return result
+
+}
+// 测试一下
+var foo = {
+  value: 1
+};
+
+function bar (name, age) {
+  console.log(name)
+  console.log(age)
+  console.log(this.value);
+}
+
+bar.myCall(foo, 'kevin', 18);
