7f9eb585e21e180f36344ee27a8aef004fe6e24f
chelseachen007 <aqwejie@qq.com>
2020-11-19 16:50:49 +0800

    feat: Vue 错误上报插件化

---
+++ my-error-monitor/frontend/vue-sample/plugin/ErrorHandlerPlugins.js
@@ -0,0 +1,38 @@
+export const ErrorHandlerPlugin = {
+    install (Vue, options = {}) {
+        Vue.config.errorHandler = (err, vm, info) => {
+            let { message, name, script = '', line = 0, column = 0, stack } = err
+            console.log('errorHandler:', err)
+            uploadError({ error: err })
+        }
+    }
+}
+
+
+
+function uploadError ({
+    lineno,
+    colno,
+    error: {
+        stack
+    },
+    timeStamp,
+    message,
+    filename
+}) {
+    // 过滤
+    const info = {
+        lineno,
+        colno,
+        stack,
+        timeStamp,
+        message,
+        filename
+    }
+
+    // const str = Base64.encode(JSON.stringify(info))
+    const str = window.btoa(JSON.stringify(info))
+    const host = 'http://localhost:7001/monitor/error'
+    new Image().src = `${host}?info=${str}`
+}
+
