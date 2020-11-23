2f5d50787cb07342d37f7879418574a7395c54e4
chelseachen007 <aqwejie@qq.com>
2020-11-23 18:35:31 +0800

    feeat: 虚拟列表

---
+++ my-virtual-list/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <VirtualList :listData="data" :estimatedItemSize="100" v-slot="slotProps">
+    <p>
+      <span style="color: red">{{ slotProps.item.id }}</span
+      >{{ slotProps.item.value }}
+    </p>
+  </VirtualList>
+</template>
+
+<script>
+import VirtualList from "./components/VirtualList.vue";
+
+import faker from "faker";
+
+let data = [];
+for (let id = 0; id < 1000; id++) {
+  data.push({
+    id,
+    value: faker.lorem.sentences(), // 长文本
+  });
+}
+
+export default {
+  name: "app",
+  data() {
+    return {
+      data,
+    };
+  },
+  components: {
+    VirtualList,
+  },
+};
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+  margin: 0;
+}
+#app {
+  height: 100%;
+}
+</style>
