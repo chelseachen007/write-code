4290ca0df48f667e2bcafc182b54204c9c18ca03
chelseachen007 <aqwejie@qq.com>
2020-11-27 11:38:52 +0800

    chore:eslint

--- my-error-monitor/frontend/vue-sample/.eslintrc.js
+++ my-error-monitor/frontend/vue-sample/.eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-
+    env: {
+        browser: true,
+        node: true,
+    },
+    plugins: ['vue', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:vue/essential', 'prettier'],
     rules: {
         'prettier/prettier': 'off',
     },
